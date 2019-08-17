fc35bb3be98b83d3cda212f0b4b9fe0ce1c7e53f
Ashutosh  Chauhan <masterashu@live.in>
2019-08-17 14:30:34 +0530

    Adding OOPs Lab

---
+++ OOPS/Lab1/accounts.cpp
@@ -0,0 +1,213 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+class Customer {
+private:
+    char name[100];
+    int age;
+    char address[250];
+    char aadhaar_no[17];
+
+public:
+    Customer(char *customer_name, int customer_age, char *customer_address, char *customer_aadhaar_no) {
+        strcpy(name, customer_name);
+        age = customer_age;
+        strcpy(address, customer_address);
+        strcpy(aadhaar_no, customer_aadhaar_no);
+    }
+    char* get_name(){
+        return name;
+    }
+    int get_age(){
+        return age;
+    }
+    char* get_address(){
+        return address;
+    }
+    char* get_aadhaar_no(){
+        return aadhaar_no;
+    }
+
+    char* set_name(char *customer_name){
+        strcpy(name, customer_name);
+    }
+    int set_age(int customer_age){
+        age = customer_age;
+    }
+    char* set_address(char *customer_address){
+        strcpy(address, customer_address);
+    }
+    char* set_aadhaar_no(char *customer_aadhaar_no){
+        strcpy(aadhaar_no, customer_aadhaar_no);
+    }
+};
+
+
+
+class BankAccount {
+private:
+    int account_no;
+    Customer *customer;
+    double balance;
+    char password[41];
+    bool logged_in;
+
+public:
+    BankAccount(int custommer_account_no, Customer *customer_detail) {
+        account_no = custommer_account_no;
+        strcpy(password, "PassWord");
+        customer = customer_detail;
+        balance = 0;
+    }
+
+    void log_in(char *customer_password){
+        if(strcmp(password, customer_password) == 0){
+            logged_in = true;
+        }
+    }
+
+    void log_out(){
+        logged_in = false;
+    }
+
+    double checkBalance(){
+        if (logged_in){
+            cout << "Current Balance: " << balance << "\n";
+            return balance;
+        }
+        else {
+            cout << "Not Logged in!\n";
+            return 0;
+        }
+    }
+
+    void deposit(double amount){
+        if (logged_in){
+            balance += amount;
+            cout << "Current Balance: " << balance << "\n";
+
+        }
+        else {
+            cout << "Not Logged in!\n";
+        }
+    }
+
+    void widthdraw(double amount){
+        if (logged_in){
+            balance -= amount;
+            cout << "Current Balance: " << balance << "\n";
+        }
+        else {
+            cout << "Not Logged in!\n";
+        }
+    }
+
+};
+
+
+class Bank {
+private:
+    char name[50];
+    long no_of_accounts;
+    
+public:
+    Bank(char *bank_name) {
+        strcpy(name, bank_name);
+        no_of_accounts = 0;
+    }
+
+    BankAccount* createAccount(Customer customer){
+        int account_no = generateBankAccountNo();
+        return new BankAccount(account_no, &customer);
+    }
+
+    int generateBankAccountNo(){
+        return ++no_of_accounts;
+    }
+
+    double calculateIntrest(BankAccount account, int time, double rate){
+        account.log_in("PassWord");
+        double balance = account.checkBalance();
+        account.log_out();
+        double intrest = (balance * rate * time) / 100;
+        return intrest;
+    }
+
+};
+
+class ATM {
+private:
+    int id;
+public:
+    ATM(int atm_id = 1){
+        id = atm_id;
+    };
+
+    double checkBalance(BankAccount account, char *password){
+        account.log_in(password);
+        account.checkBalance();
+        account.log_out();
+    }
+
+    void widthdraw(BankAccount account, char *password, double amount){
+        account.log_in(password);
+        account.widthdraw(amount);
+        account.log_out();
+    }
+};
+
+
+
+
+int main(int argc, char const *argv[])
+{
+    Bank SBI("State Bank of India");
+    ATM ATM1(1);
+
+    Customer C1("Ashutosh Chauhan", 20, "Amroha", "919297361631");
+    Customer C2("Palaash Agarwal", 29, "Agra", "919293361631");
+    Customer C3("Raahul Singh", 21, "Dehradun", "919227361631");
+
+    BankAccount *myAccount1 = SBI.createAccount(C1);
+    BankAccount *myAccount2 = SBI.createAccount(C2);
+
+    cout << "-----USING BANK ACCOUNT---------\n";
+
+    cout << "Without Logging In: \n\n";
+
+    myAccount1->deposit(10000);
+    myAccount1->widthdraw(50);
+
+    myAccount1->log_in("PassWord");
+    cout << "\nAfter Logging In: \n\n";
+
+    myAccount1->deposit(10000);
+    myAccount1->widthdraw(50);
+
+    myAccount1->log_out();
+
+    cout << "\nAfter Logging Out: \n\n";
+
+    myAccount1->deposit(10000);
+    myAccount1->widthdraw(50);
+    
+    cout << "--------------------------------------\n\n";
+
+    cout << "----------USING ATM-WRONG PASSWORD--------\n\n";
+
+    ATM1.checkBalance(*myAccount1, "Password");
+    ATM1.widthdraw(*myAccount1, "Password", 400);    
+
+    cout << "\n--------USING ATM-CORRECT-PASSWORD--------\n\n";
+
+
+    ATM1.checkBalance(*myAccount1, "PassWord");
+    ATM1.widthdraw(*myAccount1, "PassWord", 100);    
+
+    cout << "-----------------------------\n";
+
+
+    return 0;
+}
